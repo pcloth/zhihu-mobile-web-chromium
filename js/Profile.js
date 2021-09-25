ee36e313c31b5fed33fefa34f1498e4dc76a060e
pcloth <pcloth@gmail.com>
2021-09-25 09:45:11 +0800

    重构完成

--- js/Profile.js
+++ js/Profile.js
@@ -1,16 +1,66 @@
-﻿chrome.storage.local.get(['enablePc', 'optimize'], function(data){
+﻿// chrome.storage.local.get(['enablePc', 'optimize'], function(data){
+// 	if(data.enablePc && data.optimize){
+// 		$main = $("main.App-main > div", $root);
+// 		$mainColumn = $('#People-mainColumn', $main);
+// 		$sideBar = $('.Profile-sideColumn', $main);
+
+// 		insertCss();
+
+// 		//修改 应用内打开 链接
+// 		var params = window.location.pathname.split('/');
+// 		if( params[1] && params[1] === 'people' && params[2])
+// 			$(".tb-app", $leftSide).attr('href', 'zhihu://people/' + params[2]);
+// 		else
+// 			$(".tb-app", $leftSide).attr('href', 'zhihu://');
+// 	}
+// });
+
+// 用户个人主页 ver 2021-09-25
+loadOptions().then(data=>{
 	if(data.enablePc && data.optimize){
-		$main = $("main.App-main > div", $root);
-		$mainColumn = $('#People-mainColumn', $main);
-		$sideBar = $('.Profile-sideColumn', $main);
-
 		insertCss();
-
-		//修改 应用内打开 链接
-		var params = window.location.pathname.split('/');
-		if( params[1] && params[1] === 'people' && params[2])
-			$(".tb-app", $leftSide).attr('href', 'zhihu://people/' + params[2]);
-		else
-			$(".tb-app", $leftSide).attr('href', 'zhihu://');
+		addProfileCSS();
+		fixedTimeLineMobile();	
+		fixedProfile();
 	}
-});
+})
+
+function fixedProfile(){
+	document.querySelector('.ProfileButtonGroup.ProfileMain-buttons').children[0].textContent = '编辑'
+}
+
+
+function addProfileCSS(){
+	addCSSText(`
+	.ProfileHeader-buttons {
+		position: inherit;
+	}
+
+	.ProfileHeader-info {
+		display: flex;
+		align-items: center;
+	}
+
+	.ProfileHeader-contentBody {
+		height: auto!important;
+	}
+
+	.ProfileHeader-infoItem:not(:last-child) {
+		margin-bottom: 0;
+	}
+
+	.ProfileHeader-title {
+		flex-direction: column;
+	}
+
+	.ProfileHeader-headline {
+		margin-left: 0;
+		font-size: 14px;
+	}
+
+	.PageHeader .ProfileMain-header {
+		width: calc(100% - 86px);
+		overflow: scroll;
+	}
+	`)
+}
--- js/common.js
+++ js/common.js
@@ -5,6 +5,34 @@ function insertCss() {
 
 }
 
+function addCSSText(cssText){
+    let style = document.createElement('style'),  //创建一个style元素
+        head = document.head || document.getElementsByTagName('head')[0]; //获取head元素
+    style.type = 'text/css'; //这里必须显示设置style元素的type属性为text/css，否则在ie中不起作用
+    if(style.styleSheet){ //IE
+        let func = function(){
+            try{ //防止IE中stylesheet数量超过限制而发生错误
+                style.styleSheet.cssText = cssText;
+            }catch(e){
+
+            }
+        }
+        //如果当前styleSheet还不能用，则放到异步中则行
+        if(style.styleSheet.disabled){
+            setTimeout(func,10);
+        }else{
+            func();
+        }
+    }else{ //w3c
+        //w3c浏览器中只要创建文本节点插入到style元素中就行了
+        let textNode = document.createTextNode(cssText);
+        style.appendChild(textNode);
+    }
+    head.appendChild(style); //把创建的style元素插入到head中    
+}
+
+
+
 // 处理顶部搜索栏目
 function fixedSearchInput(){
 	// 给搜索表单添加事件缩放尺寸
